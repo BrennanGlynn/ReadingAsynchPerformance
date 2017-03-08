10b6a9eb193e8d90b9bf160be28912a0e377393b
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-08 12:18:56 -0700

    Took notes from ch8 file and correctly put them in ch1

--- Ch8_Advance_Async_Patterns.js
+++ Ch8_Advance_Async_Patterns.js
@@ -1,10 +1,3 @@
 /**
- * Created by brenn on 3/8/2017.
- */
-var data = ajax("http://some.url.1");
-
-console.log(data);
-// Generally the line above will run before the ajax request returns a value
-// Ajax calls are not synchronous
-// This means it will not wait for a return value before continuing
-// We make requests "NOW" and receive them "LATER"
+* Created by brenn on 3/8/2017.
+*/
