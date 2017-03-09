38679e79571d555954f52e8b4139216df4ee5294
Brennan <brennanglynn@u.boisestate.edu>
2017-03-09 12:41:31 -0700

    Chapter 2 completed deeper this time

--- Ch2_Callbacks.js
+++ Ch2_Callbacks.js
@@ -69,6 +69,110 @@ This is a lot to worry about for every single callback you pass to a utility tha
 //                     Not Just Others' Code                    //
 //////////////////////////////////////////////////////////////////
 
-// we're on page 34 gonna eat lunch
+// Can you trust your own utilities?
+
+function addNumbers(x,y) {
+    return x + y;
+}
+
+console.log(addNumbers(21,21));
+// => 42
+
+console.log(addNumbers("21","21"));
+// => "2121"
+
+// We could
+
+function add(x, y) {
+    x = Number(x);
+    y = Number(y);
+    return x + y;
+}
+
+console.log(add(3, 4)); // => 7
+console.log(add("1", "3")); // => 4
+console.log(add("1", 2)); // =>3
+
+// Trust, but verify
+
+/*
+ If you have code that uses callbacks, especially but not exclusively with third-party utilities,
+ and you're not already applying some sort of mitigation logic for all these inversion of control
+ trust issues, your code has bugs in it right now even though they may not have bitten you yet.
+ Latent bugs are still bugs.
+*/
+
+//////////////////////////////////////////////////////////////////
+//                   Trying to Save Callbacks                   //
+//////////////////////////////////////////////////////////////////
+
+/*
+A common callback pattern is called "error-first style" sometimes called "Node Style"
+The first argument of a single callback is reserved for an error object
+If error will pass a truthy value for first argument
+ */
+
+// function response(err, data) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// }
+//
+// ajax( "http://website.url.1", response );
+// ajax( "http://website.url.2", response );
+
+/*
+Several problems:
+    Nothing prevents repeated invocations (could use a once decorator though)
+    You might get an error and success or even no error signal
+    Doesn't have much reuse so will have to repeat for every callback
+
+    What if the response never get's called? Now we have to add a timeout
+ */
+
+// Don't release Zalgo!
+/*
+Always invoke callbacks asynchronously
+even if it's right away on the event loop
+
+This way all callbacks are predictably async
+*/
+
+// Consider
+
+// function result(data) {
+//     console.log(a);
+// }
+//
+// var a = 0;
+//
+// ajax("..pre-cached-url..", result);
+// a++;
+/*
+This code could print 0, or 1 depending on conditions
+To fix we need more bloated boilerplate to weigh the project down
+This is the story with callbacks. They can do anything, but you have
+to work hard to get it to work
+
+*/
+
+//////////////////////////////////////////////////////////////////
+//                            Review                            //
+//////////////////////////////////////////////////////////////////
+
+/*
+Callbacks are fundamental, but not enough for the future
+Brains plan things sequentially in a single thread
+Callbacks express asynchronous behavior nonlinearly which makes code
+hard to reason. This can lead to bad bugs.
+
+We need a way to express asynchrony in a synchronous manner
+Callbacks suffer from inversion of control leading to trust issues
+Inventing logic to solve these is possible, but difficult and clunky
+
+We need a generalized solution. (promises and generators wink* wink*)
+*/
 
 
