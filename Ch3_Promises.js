512ece9715da3447b0120a6d4fa8eea623cf4bf4
Brennan <brennanglynn@u.boisestate.edu>
2017-03-09 13:00:13 -0700

    Introduction to Promises

--- Ch3_Promises.js
+++ Ch3_Promises.js
@@ -1,3 +1,17 @@
-/**
- * Created by brenn on 3/8/2017.
- */
+//////////////////////////////////////////////////////////////////
+//                          Chapter 3                           //
+//                           Promises                           //
+//////////////////////////////////////////////////////////////////
+
+/*
+What if instead of handing the continuation of our program to another
+party, we could expect it to return us a capability to know when its task finishes,
+and then our code could decide what to do next?
+
+Well that's exactly what promises are.
+*/
+
+//////////////////////////////////////////////////////////////////
+//                      What is a Promise?                      //
+//////////////////////////////////////////////////////////////////
+// Well, guess I got to go to class right now, brb
