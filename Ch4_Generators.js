d3952192075c9e8c7623185797e12391a49d6b0b
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-11 16:49:37 -0700

    Delegating Messages

--- Ch4_Generators.js
+++ Ch4_Generators.js
@@ -665,5 +665,51 @@ of the details of asynchrony away from that code as possible.
 // Note: yield * yields iteration control
 
 //////////////////////////////////////////////////////////////////
-//                        Why Delegation?                       //
+//                      Delegating Messages                     //
+//////////////////////////////////////////////////////////////////
+
+(function () {
+    function *foo() {
+        console.log("inside `*foo()`:", yield "B");
+
+        console.log("inside `*foo()`:", yield "C");
+
+        return "D";
+    }
+
+    function *bar() {
+        console.log("inside `*bar()`:", yield "A");
+
+        // `yield`-delegation!
+        console.log("inside `*bar()`:", yield *foo());
+
+        console.log("inside `*bar()`:", yield "E");
+
+        return "F";
+    }
+
+    let it = bar();
+    console.log("outside:", it.next().value);
+    // outside: A
+
+    console.log("outside:", it.next(1).value);
+    // inside `*bar()`: 1
+    // outside: B
+
+    console.log("outside:", it.next(2).value);
+    // inside `*foo()`: 2
+    // outside: C
+
+    console.log("outside:", it.next(3).value);
+    // inside `*foo()`: 3
+    // inside `*bar()`: D
+    // outside: E
+
+    console.log("outside:", it.next(4).value);
+    // inside `*bar()`: 4
+    // outside: F
+})();
+
+//////////////////////////////////////////////////////////////////
+//                  Exceptions Delegated, Too!                  //
 //////////////////////////////////////////////////////////////////
