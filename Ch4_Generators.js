6816fcae407708b97323fb78a5d3cf1c473bebba
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-11 17:20:12 -0700

    Exceptions Delegated, Too!

--- Ch4_Generators.js
+++ Ch4_Generators.js
@@ -668,48 +668,110 @@ of the details of asynchrony away from that code as possible.
 //                      Delegating Messages                     //
 //////////////////////////////////////////////////////////////////
 
-(function () {
-    function *foo() {
-        console.log("inside `*foo()`:", yield "B");
-
-        console.log("inside `*foo()`:", yield "C");
-
-        return "D";
-    }
-
-    function *bar() {
-        console.log("inside `*bar()`:", yield "A");
-
-        // `yield`-delegation!
-        console.log("inside `*bar()`:", yield *foo());
-
-        console.log("inside `*bar()`:", yield "E");
-
-        return "F";
-    }
-
-    let it = bar();
-    console.log("outside:", it.next().value);
-    // outside: A
-
-    console.log("outside:", it.next(1).value);
-    // inside `*bar()`: 1
-    // outside: B
-
-    console.log("outside:", it.next(2).value);
-    // inside `*foo()`: 2
-    // outside: C
-
-    console.log("outside:", it.next(3).value);
-    // inside `*foo()`: 3
-    // inside `*bar()`: D
-    // outside: E
-
-    console.log("outside:", it.next(4).value);
-    // inside `*bar()`: 4
-    // outside: F
-})();
+// (function () {
+//     function *foo() {
+//         console.log("inside `*foo()`:", yield "B");
+//
+//         console.log("inside `*foo()`:", yield "C");
+//
+//         return "D";
+//     }
+//
+//     function *bar() {
+//         console.log("inside `*bar()`:", yield "A");
+//
+//         // `yield`-delegation!
+//         console.log("inside `*bar()`:", yield *foo());
+//
+//         console.log("inside `*bar()`:", yield "E");
+//
+//         return "F";
+//     }
+//
+//     let it = bar();
+//     console.log("outside:", it.next().value);
+//     // outside: A
+//
+//     console.log("outside:", it.next(1).value);
+//     // inside `*bar()`: 1
+//     // outside: B
+//
+//     console.log("outside:", it.next(2).value);
+//     // inside `*foo()`: 2
+//     // outside: C
+//
+//     console.log("outside:", it.next(3).value);
+//     // inside `*foo()`: 3
+//     // inside `*bar()`: D
+//     // outside: E
+//
+//     console.log("outside:", it.next(4).value);
+//     // inside `*bar()`: 4
+//     // outside: F
+// })();
 
 //////////////////////////////////////////////////////////////////
 //                  Exceptions Delegated, Too!                  //
+//////////////////////////////////////////////////////////////////
+
+/*
+In the same way that yield -delegation transparently passes messages through in both directions, errors/exceptions also
+pass in both directions
+*/
+(function () {
+    function *foo() {
+        try {
+            yield "B";
+        }
+        catch (err) {
+            console.log("error caught inside '*foo():", err);
+        }
+        yield "C";
+        yield "D";
+    }
+    function *bar() {
+        yield "A";
+
+        try {
+            yield *foo();
+        }
+        catch (err) {
+            console.log( "error caught inside '*bar()':", err);
+        }
+        yield "E";
+        yield *baz();
+        // note: can't get here!
+        yield "G";
+    }
+    function *baz() {
+        throw "F";
+    }
+
+    let it = bar();
+
+    console.log( "outside:", it.next().value );
+    // outside: A
+
+    console.log( "outside:", it.next( 1 ).value );
+    // outside: B
+
+    console.log( "outside:", it.throw( 2 ).value );
+    // error caught inside `*foo()`: 2
+    // outside: C
+
+    console.log( "outside:", it.next( 3 ).value );
+    // error caught inside `*bar()`: D
+    // outside: E
+
+    try {
+        console.log( "outside:", it.next( 4 ).value );
+    }
+    catch (err) {
+        console.log( "error caught outside:", err );
+    }
+    // error caught outside: F
+})();
+
+//////////////////////////////////////////////////////////////////
+//                     Generator Concurrency                    //
 //////////////////////////////////////////////////////////////////
